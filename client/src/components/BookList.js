af1ea6398e4b9ceabf9d6d4685c10912a2603234
neetika-Sri <neetika.srivastava@gmail.com>
2020-06-04 16:13:00 +1200

    push all

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+function BookList() {
+	return (
+		<div>
+			<ul id="book list">
+				<li>Book Name</li>
+			</ul>
+		</div>
+	);
+}
+
+export default BookList;
